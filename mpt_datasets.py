14f6b68ddd5b91d50a1b6adc1946778e2d1db7df
Ludovic Denoyer <ludovic.denoyer@lip6.fr>
2018-07-19 10:33:40 +0200

    coucou

--- mpt_datasets.py
+++ mpt_datasets.py
@@ -8,6 +8,99 @@ import random
 from torchvision import datasets, transforms
 import torch.utils.data
 
+def extract_coco_images(cocodir,cocoinstances,categories=[],proportion=1.0):
+	'''
+	Exract images from coco given a list of categories. Return is list of [(image, annotations)]
+	:param cocodir:
+	:param cocoinstances:
+	:param categories:
+	:param proportion:
+	:return:
+	'''
+	transform=transforms.Compose([transforms.Resize((28*4,28*4)),transforms.ToTensor()])
+	train_dataset=datasets.CocoDetection(cocodir,cocoinstances,transform=transform)
+
+
+	#Read categories
+	CATEGORIES_IDS_TO_KEEP=[]
+	for c in train_dataset.coco.dataset["categories"]:
+		if (c['name'] in categories):
+			CATEGORIES_IDS_TO_KEEP.append(c['id'])
+
+	nb_kept=0
+	nb_total=0
+	images=[]
+	for idx in range(len(train_dataset)):
+		y=train_dataset.ids[idx]
+		y = train_dataset.coco.getAnnIds(imgIds=y)
+		y = train_dataset.coco.loadAnns(y)
+		ids=[]
+		for a in y:
+			if (not a["category_id"] in ids):
+				ids.append(a["category_id"])
+		keep=False
+		for c in ids:
+			if (c in CATEGORIES_IDS_TO_KEEP):
+				keep=True
+
+		if (keep):
+			if (random.random()<proportion):
+				x, z = train_dataset[idx]
+				img_id = train_dataset.ids[idx]
+				path = train_dataset.coco.loadImgs(img_id)[0]['file_name']
+				images.append((x,ids))
+				print(ids)
+
+
+
+	logging.info("found %d images"%len(images))
+	return(images)
+
+def load_unsupervised_images(images,size_batches=64,nb_batches=10):
+	'''
+	Takes a list of images and build batches
+	:param images:
+	:param size_batches:
+	:param nb_batches:
+	:return:
+	'''
+	batches=[]
+	for n in range(nb_batches):
+		batch=torch.Tensor(images[0].size())
+		batch=batch.unsqueeze(0)
+		batch=batch.repeat(size_batches,1,1,1)
+		for i in range(size_batches):
+			idx=random.randint(0,len(images)-1)
+			batch[i]=images[idx]
+		batches.append(batch)
+	return batches
+
+
+def load_supervised_images(images,size_batches=64,nb_batches=10):
+	'''
+	Takes a list of images and categories and build batches. Categories are indexes between 0 and C-1
+	:param images:
+	:param size_batches:
+	:param nb_batches:
+	:return:
+	'''
+	batches=[]
+	for n in range(nb_batches):
+		batch=torch.Tensor(images[0][0].size())
+		batch_y = torch.Tensor(size_batches)
+
+		batch=batch.unsqueeze(0)
+		batch=batch.repeat(size_batches,1,1,1)
+		for i in range(size_batches):
+			idx=random.randint(0,len(images)-1)
+			batch[i]=images[idx][0]
+			print(images[idx][1])
+			batch_y[i] = images[idx][1]
+
+		batches.append((batch,batch_y))
+	return batches
+
+
 def load_unsupervised_mnist(size_batches=64,sampling_rate=1.0):
 	'''
 	Load batches of numbers from MNIST (train)
@@ -27,6 +120,28 @@ def load_unsupervised_mnist(size_batches=64,sampling_rate=1.0):
 			all_examples.append(data)
 
 	logging.info("\t%d batches of size %d built"%(len(all_examples),size_batches))
+	return all_examples
+
+
+def load_supervised_mnist(size_batches=64, sampling_rate=1.0):
+	'''
+	Load batches of numbers from MNIST (train)
+	'''
+
+	logging.info("Creating mini-batches from MNIST")
+	loader = torch.utils.data.DataLoader(
+		datasets.MNIST(root='.', train=True, download=True,
+					   transform=transforms.Compose([
+						   transforms.ToTensor(),
+						   transforms.Normalize((0.1307,), (0.3081,))
+					   ])), batch_size=size_batches, shuffle=True, num_workers=0)
+
+	all_examples = []
+	for batch_idx, (data, target) in enumerate(loader):
+		if (random.random() < sampling_rate):
+			all_examples.append((data,target))
+
+	logging.info("\t%d batches of size %d built" % (len(all_examples), size_batches))
 	return all_examples
     
 def load_unsupervised_mnist_random_position(size_batches=64,sampling_rate=1.0):
@@ -127,6 +242,13 @@ def load_unsupervised_mnist_two_digits(size_batches=64,nb_batches=100,digits=[0,
 	return(batches)
 		
 
+def sample_batches(batches,proportion):
+    retour=[]
+    for aa in batches:
+        if (random.random()<proportion):
+            retour.append(aa)
+
+    return retour
 
    
 
--- mpt_vision_modules.py
+++ mpt_vision_modules.py
@@ -2,6 +2,35 @@
 
 import torch.nn as nn
 import torch
+from torch.autograd import Function
+
+def inverse_stn(stn_matrix):
+	'''
+	Get a Bx2x3 set of grids, and returns grids corresponding to the inverse transformation
+	:param stn_matrix:
+	:return:
+	'''
+	A=stn_matrix.narrow(2,0,2)
+	B=stn_matrix.narrow(2,2,1)
+	inverse_A = [t.inverse() for t in torch.functional.unbind(A)]
+	inverse_A = torch.stack(inverse_A, dim=0)
+	inverse_AB = torch.bmm(inverse_A, B)
+	inverse_stn = torch.cat([inverse_A, -inverse_AB], dim=2)
+	return inverse_stn
+
+class GradReverse(Function):
+    def __init__(self, lambd):
+        self.lambd = lambd
+
+    def forward(self, x):
+        return x.view_as(x)
+
+    def backward(self, grad_output):
+        return (grad_output * -self.lambd)
+
+def grad_reverse(x, lambd=1.0):
+    return GradReverse(lambd)(x)
+
 
 def image_to_vector_module(image_size,*args):
 	'''
@@ -69,7 +98,38 @@ def image_to_vector_module(image_size,*args):
 			x=x.view(-1,self.os)		
 			return x
 
+	class Convolution_COCO_112(nn.Module):
+		'''
+        input size is 1x28x28 (MNIST)
+        output size is 10x3x3
+        '''
 
+		def __init__(self):
+			nn.Module.__init__(self)
+			self.convol = nn.Sequential(
+				nn.BatchNorm2d(3),
+				nn.Conv2d(3, 8, kernel_size=7),
+				nn.MaxPool2d(2, stride=2),
+				nn.ReLU(True),
+				nn.BatchNorm2d(8),
+				nn.Conv2d(8, 10, kernel_size=5),
+				nn.MaxPool2d(2, stride=2),
+				nn.ReLU(True),
+				nn.BatchNorm2d(10),
+				nn.Conv2d(10, 10, kernel_size=5),
+				nn.MaxPool2d(2, stride=2),
+				nn.ReLU(True)
+			)
+			self.os = 0
+
+		def forward(self, x):
+			x = self.convol(x)
+			if (self.os == 0):
+				self.os = 1
+				for i in range(1, len(x.size())):
+					self.os *= x.size()[i]
+			x = x.view(-1, self.os)
+			return x
 
 	image_z=image_size[0]
 	sx=image_size[1]
@@ -79,9 +139,9 @@ def image_to_vector_module(image_size,*args):
 		return Convolution_MNIST(),90
 	if ((image_z==1) and (sx==56) and (sy==56)):
 		return Convolution_MNIST_56(),90
+	if ((image_z == 3) and (sx == 112) and (sy == 112)):
+		return Convolution_COCO_112(),1000
 
-
-	
 
 class STN_Square(nn.Module):
     #Takes a Bx1 localisation + Bx2 translation and returns a Bx2x3 grid
@@ -110,6 +170,35 @@ class STN_Square(nn.Module):
 #
         theta = torch.cat([localization,translation.unsqueeze(-1)],dim=2)
         return theta
-       
+
+class Coords_To_Affine(nn.Module):
+    #Coords are between -1 and +1
+	#Assuming that image_size is an int (square images)
+	def __init__(self):
+		super(Coords_To_Affine, self).__init__()
+		self.mask=torch.ones(2,2)
+		self.mask[0][1]=0
+		self.mask[1][0]=0
+		self.mask=self.mask.unsqueeze(0)
+
+		# Spatial transformer network forward function
+	#Coords is a Bx2 vector => (x1,y1) (x2,y2)
+	def forward(self, xy1,xy2):
+
+		dm = self.mask.device
+		ds = xy1.device
+		if (dm != ds):
+			self.mask = self.mask.to(ds)
+
+		B=(xy1+xy2)/2
+
+		A=(xy2-xy1)/2
+		A=A.unsqueeze(-1).repeat(1,1,2)
+		A=A*self.mask
+		AB=torch.cat([A, B.unsqueeze(-1)], dim=2)
+
+		return AB
+
+
 	
  
